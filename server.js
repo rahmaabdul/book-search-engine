1ec1833110cde71efff6d6da1bc06763c4acf59c
Rahma Abdullahi <82878278+rahmaabdul@users.noreply.github.com>
2021-11-25 19:58:15 +0000

    debug

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const path = require("path");
 const db = require("./server/config/connection");
-const routes = require("./server/routes");
+const routes = require("./routes");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+require("dotenv").config();
 
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === "production") {

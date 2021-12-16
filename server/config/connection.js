de32dedb91b92ac8ae507e884a8620d22efd2f7f
Rahma Abdullahi <82878278+rahmaabdul@users.noreply.github.com>
2021-12-16 02:50:45 +0000

    mongodb and heroku

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,11 +2,14 @@ const mongoose = require("mongoose");
 
 require("dotenv").config("../");
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-});
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/booksearchengine",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  }
+);
 
 module.exports = mongoose.connection;
